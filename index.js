06d162e7cc9c54aa8a95f7f166dbac263413b9d4
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-09-13 06:45:23 -0400

    tired

--- index.js
+++ index.js
@@ -94,13 +94,16 @@ console.log(getAverageGoals(getFinals(fifaData)));
 Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
 
-function getCountryWins( /* code here */ ) {
-
-    /* code here */
-
+function getCountryWins(data, teamInitials) {
+    const count = {};
+    for (let i = 0; i < data.length; i++) {
+        let num = data[i];
+        count[num] = count[num] ? count[num] + 1 : 1;
+    }
+    return count;
 };
 
-getCountryWins();
+console.log(getCountryWins(getWinners(fifaData), 'ITA'));
 
 
 /* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
